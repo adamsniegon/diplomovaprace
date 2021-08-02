1c956bf1e0a68d65f8dc7b69b797efddbbb0f814
Adam <a.sniegon@seznam.cz>
2021-08-02 11:45:50 +0200

    feat: Add lightbox for place images

--- components/DetailGallery.js
+++ components/DetailGallery.js
@@ -1,12 +1,18 @@
 import Image from 'next/image';
 import styles from '../styles/DetailGallery.module.css';
+import { SRLWrapper } from 'simple-react-lightbox';
+import SimpleReactLightbox from 'simple-react-lightbox';
 
 export default function DetailGallery({images}) {
     return (
-        <div className={styles.detailGallery}>
-            {images.map(image => (
-                <Image className={styles.detailGallery__image} src={image.url} width={image.width} height={image.height}/>
-            ))}
-        </div>
+        <SimpleReactLightbox>
+            <SRLWrapper>
+                <div className={styles.detailGallery}>
+                    {images.map(image => (
+                        <Image className={styles.detailGallery__image} src={image.url} width={image.width} height={image.height}/>
+                    ))}
+                </div>
+            </SRLWrapper>
+        </SimpleReactLightbox>
     )
 }
