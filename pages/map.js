806b2784dc1983ea3c06d465becee8133c034f98
Adam <a.sniegon@seznam.cz>
2021-08-07 00:18:51 +0200

    feat: Add margin for cards in map list

--- pages/map.js
+++ pages/map.js
@@ -23,9 +23,8 @@ export default function Map() {
             </Head>
             <MapLayout>
                 <div className={styles.mapList}>
-                    {error && <div>failed to load</div>}
-                    {!places && <div>loading...</div>}
                     {places && places.map(({id, name, description_short, url, city: {name: cityName}, image}) => (
+                        <>
                         <Link key={id} href={`/places/${url}`}>
                             <a className={styles.mapList__link}>
                                 <div className={styles.mapList__card}>
@@ -40,6 +39,63 @@ export default function Map() {
                                 </div>
                             </a>
                         </Link>
+                        <Link key={id} href={`/places/${url}`}>
+                            <a className={styles.mapList__link}>
+                                <div className={styles.mapList__card}>
+                                    <div className={styles.mapList__cardImageWrapper}>
+                                        <Image className={styles.mapList__cardImage} src={image[0].url} width={image[0].width} height={image[0].height}/>
+                                    </div>
+                                    <div>
+                                        <h1 className={styles.mapList__cardHeadline}>{name}</h1>
+                                        <p className={styles.mapList__cardDescription} title={description_short}>{shortDescription(description_short)}</p>
+                                        <p className={styles.mapList__cardCityName}><img className={styles.mapList__cardIcon} src="/icon-pin.svg"></img>{cityName}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </Link>
+                        <Link key={id} href={`/places/${url}`}>
+                            <a className={styles.mapList__link}>
+                                <div className={styles.mapList__card}>
+                                    <div className={styles.mapList__cardImageWrapper}>
+                                        <Image className={styles.mapList__cardImage} src={image[0].url} width={image[0].width} height={image[0].height}/>
+                                    </div>
+                                    <div>
+                                        <h1 className={styles.mapList__cardHeadline}>{name}</h1>
+                                        <p className={styles.mapList__cardDescription} title={description_short}>{shortDescription(description_short)}</p>
+                                        <p className={styles.mapList__cardCityName}><img className={styles.mapList__cardIcon} src="/icon-pin.svg"></img>{cityName}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </Link>
+                        <Link key={id} href={`/places/${url}`}>
+                            <a className={styles.mapList__link}>
+                                <div className={styles.mapList__card}>
+                                    <div className={styles.mapList__cardImageWrapper}>
+                                        <Image className={styles.mapList__cardImage} src={image[0].url} width={image[0].width} height={image[0].height}/>
+                                    </div>
+                                    <div>
+                                        <h1 className={styles.mapList__cardHeadline}>{name}</h1>
+                                        <p className={styles.mapList__cardDescription} title={description_short}>{shortDescription(description_short)}</p>
+                                        <p className={styles.mapList__cardCityName}><img className={styles.mapList__cardIcon} src="/icon-pin.svg"></img>{cityName}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </Link>
+                        <Link key={id} href={`/places/${url}`}>
+                            <a className={styles.mapList__link}>
+                                <div className={styles.mapList__card}>
+                                    <div className={styles.mapList__cardImageWrapper}>
+                                        <Image className={styles.mapList__cardImage} src={image[0].url} width={image[0].width} height={image[0].height}/>
+                                    </div>
+                                    <div>
+                                        <h1 className={styles.mapList__cardHeadline}>{name}</h1>
+                                        <p className={styles.mapList__cardDescription} title={description_short}>{shortDescription(description_short)}</p>
+                                        <p className={styles.mapList__cardCityName}><img className={styles.mapList__cardIcon} src="/icon-pin.svg"></img>{cityName}</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </Link>
+                        </>
                     ))}
                 </div>
                 <MapPage/>
