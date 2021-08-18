a3ac126750804051ff82fbc13dd3902d3c797c12
Adam <a.sniegon@seznam.cz>
2021-08-18 16:55:17 +0200

    fix: Edit Footer text

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ import styles from '../styles/Footer.module.css';
 export default function Footer() {
     return(
         <div className={styles.footer}>
-            <p className={styles.footer__text}>Tato aplikace vzikla v roce 2021 jako diplomová práce a součást projektu Interaktivní turistická mapa Karvinska na Obchodně podnikatelské fakultě v Karviné. Veškerá data použita v této aplikaci pochází z online katalogu Národního památkového ústavu.</p>
+            <p className={styles.footer__text}>Tato aplikace vzikla v roce 2021 jako diplomová práce a součást projektu Interaktivní turistická mapa Karvinska na Obchodně podnikatelské fakultě v Karviné. Většina dat použitých v této aplikaci pochází z online katalogu Národního památkového ústavu, pro rozšíření popisu některých místo byly použity jiné internetové zdroje.</p>
         </div>
     );
 }
